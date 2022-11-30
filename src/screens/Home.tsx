4649672388cedfdcd7f4b3c8115e1f9a0425216b
Mirko kawa <mirkokawa7@gmail.com>
2022-11-30 05:10:36 +0300

    create calculator
    calculator

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,12 +1,143 @@
-import React from "react"
-import { Text, View } from "react-native"
+import React from "react";
+import { Text, View, Button, TouchableOpacity } from "react-native";
+import { Appbar } from "react-native-paper";
 
 const Home = () => {
   return (
-    <View className="flex-1 items-center justify-center bg-green-300">
-      <Text>Vandam bro xoty uuuu!</Text>
-    </View>
-  )
-}
+    <View className="w-full h-full bg-black justify-end  items-center  ">
+      <View className="mb-20 space-y-5">
+        <View className="flex-row justify-center space-x-4 items-center font-medium text-4xl">
+          <TouchableOpacity
+            onPress={() => {}}
+            className="p-4 bg-[#9F9F9F] rounded-full justify-center items-center  w-20 h-20"
+          >
+            <Text className="text-black text-center text-4xl">AC</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => {}}
+            className="p-4 bg-[#9F9F9F]  rounded-full justify-center items-center  w-20 h-20"
+          >
+            <Text className="text-black text-center text-4xl">+/-</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => {}}
+            className="p-4 bg-[#9F9F9F] rounded-full justify-center items-center  w-20 h-20"
+          >
+            <Text className="text-black text-center text-4xl">%</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => {}}
+            className="p-4 bg-[#F69A06] rounded-full justify-center items-center  w-20 h-20"
+          >
+            <Text className="text-white text-center text-4xl">±</Text>
+          </TouchableOpacity>
+        </View>
 
-export default Home
+        <View className="flex-row justify-center space-x-4 items-center font-medium text-4xl">
+          <TouchableOpacity
+            onPress={() => {}}
+            className="p-4 bg-white rounded-full justify-center items-center  w-20 h-20"
+          >
+            <Text className="text-black text-center text-4xl">7</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => {}}
+            className="p-4 bg-white rounded-full justify-center items-center  w-20 h-20"
+          >
+            <Text className="text-black text-center text-4xl">8</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => {}}
+            className="p-4 bg-white rounded-full justify-center items-center  w-20 h-20"
+          >
+            <Text className="text-black text-center text-4xl">9</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => {}}
+            className="p-4 bg-[#F69A06] rounded-full justify-center items-center  w-20 h-20"
+          >
+            <Text className="text-white text-center text-4xl">*</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View className="flex-row justify-center space-x-4 items-center font-medium text-4xl">
+          <TouchableOpacity
+            onPress={() => {}}
+            className="p-4 bg-white rounded-full justify-center items-center  w-20 h-20"
+          >
+            <Text className="text-black text-center text-4xl">4</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => {}}
+            className="p-4 bg-white rounded-full justify-center items-center  w-20 h-20"
+          >
+            <Text className="text-black text-center text-4xl">5</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => {}}
+            className="p-4 bg-white rounded-full justify-center items-center  w-20 h-20"
+          >
+            <Text className="text-black text-center text-4xl">6</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => {}}
+            className="p-4 bg-[#F69A06] rounded-full justify-center items-center  w-20 h-20"
+          >
+            <Text className="text-white text-center text-4xl">-</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View className="flex-row justify-center space-x-4 items-center font-medium text-4xl">
+          <TouchableOpacity
+            onPress={() => {}}
+            className="p-4 bg-white rounded-full justify-center items-center  w-20 h-20"
+          >
+            <Text className="text-black text-center text-4xl">1</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => {}}
+            className="p-4 bg-white rounded-full justify-center items-center  w-20 h-20"
+          >
+            <Text className="text-black text-center text-4xl">2</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => {}}
+            className="p-4 bg-white rounded-full justify-center items-center  w-20 h-20"
+          >
+            <Text className="text-black text-center text-4xl">3</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => {}}
+            className="p-4 bg-[#F69A06] rounded-full justify-center items-center  w-20 h-20"
+          >
+            <Text className="text-white text-center text-4xl">+</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View className="flex-row justify-center space-x-4 items-center font-medium text-4xl">
+          <TouchableOpacity
+            onPress={() => {}}
+            className="p-4 bg-white rounded-full justify-center items-start  w-44 h-20"
+          >
+            <Text className="text-black text-center text-4xl">0</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => {}}
+            className="p-4 bg-white rounded-full justify-center items-center  w-20 h-20"
+          >
+            <Text className="text-black text-center text-4xl">.</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            onPress={() => {}}
+            className="p-4 bg-[#F69A06] rounded-full justify-center items-center  w-20 h-20"
+          >
+            <Text className="text-white text-center text-4xl">+</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default Home;
